2b3c8f6c9365fca5be613069d1eb472e1e7668ef
Justin Matis <justin.matis@gmail.com>
2016-09-21 15:17:50 -0600

    add click to create marker

---
+++ public/js/scripts.js
@@ -0,0 +1,110 @@
+/**
+ * scripts.js
+ *
+ * Computer Science 50
+ * Final Project
+ *
+ * Global JavaScript.
+ */
+// Globals
+var map;
+var viewMarkers = [];
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+      // feature. People can enter geographical searches. The search box will return a
+      // pick list containing a mix of places and predicted search terms.
+
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+ 
+      function initAutocomplete() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 38.532138, lng: -105.992014},
+            zoom: 13,
+            mapTypeId: 'roadmap'
+        });
+
+        // Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+        
+        // Listen for map clicks
+        map.addListener('click', function(event) {
+            returnLocation(event);
+            });
+        
+        
+      }
+// When user clicks on map, marker is created and then triggers viewshed calculation
+function returnLocation(event) {
+    var latLng = event.latLng;
+    createMarker(latLng);
+};
+
+
+function createMarker(latLng) {
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+    });
+    viewMarkers.push(marker)
+    // TODO: Send latlng to viewshed calculator.
+    
+};
+
